73848668d6f98dd9a531c6f9281e71ec10faf96b
Marina882 <124601675+Marina882@users.noreply.github.com>
2023-02-07 15:56:57 +0300

    Add files via upload

---
+++ giu.py
@@ -0,0 +1,11 @@
+sides = [2, 1, 5]
+a = sides[0]
+b = sides[1]
+c = sides[-1]
+D = ((b*b) - (4*a*c))
+if D >= 0:
+    x1 = ((-b + D ** (1/2)) / (2*a))
+    x2 = ((-b - D ** (1/2)) / (2*a))
+    print(x1, x2)
+else:
+    print("Корней нет")
