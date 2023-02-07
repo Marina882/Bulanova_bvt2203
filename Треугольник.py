58dbce2a273e0f71ceafc24ebac32343fc7157c1
Marina882 <124601675+Marina882@users.noreply.github.com>
2023-02-07 15:59:32 +0300

    Delete Треугольник.py

--- Треугольник.py
+++
@@ -1,9 +0,0 @@
-sides = [1, 4, 3]
-a = sides[0]
-b = sides[1]
-c = sides[-1]
-D = ((b*b) - (4*a*c))
-if D >= 0:
-    x1 = ((-b + D ** (1/2)) / (2*a))
-    x2 = ((-b - D ** (1/2)) / (2*a))
-print(x1, x2)
