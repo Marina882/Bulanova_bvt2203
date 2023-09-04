842805107ee72bf1b1b8593e537d5e44cccfbfd9
Marina882 <bulmar2003@gmail.com>
2023-09-04 19:14:15 +0300

    Второе изменение

--- laba_22.py
+++ laba_22.py
@@ -14,9 +14,6 @@ data = res.json()
 # выводим необходимые параметры
 print("Город:", s_city)
 print("Погодные условия:", data['weather'][0]['description'])
-print("Температура:", data['main']['temp'])
-print("Минимальная температура:", data['main']['temp_min'])
-print("Максимальная температура", data['main']['temp_max'])
 print("Скорость ветра:", data['wind']['speed'])
 print("Видимость:", data['visibility'])
 
@@ -26,8 +23,6 @@ print("Видимость:", data['visibility'])
 res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
 params={'q': s_city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
 data = res.json()
-
-
 
 
 
